be748ae9f0c903935f27010bbf2fabc0fcd0609a
jackpan <826647196@qq.com>
2021-06-07 13:14:56 +0800

    Initial commit

---
+++ src/com/jackpan/jvm/chapter03/FinalizeEscapeGC.java
@@ -0,0 +1,45 @@
+package com.jackpan.jvm.chapter03;
+
+/**
+ * @author jackpan
+ * @version v1.0 2021/6/7 13:02
+ */
+public class FinalizeEscapeGC {
+
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+
+    public void isAlive() {
+        System.out.println("yes, I am still alive:)");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Finalize method executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        SAVE_HOOK = null;
+        System.gc();
+
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("No, I am dead:(");
+        }
+
+        SAVE_HOOK = null;
+        System.gc();
+
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("No, I am dead:(");
+        }
+    }
+}
